53eb3b0d040cdba7aed97eb163329754976d0b7b
j339nogu <j520339a@mails.cc.ehime-u.ac.jp>
2024-06-19 11:52:06 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php 
+  foreach(file('/usr/share/dict/words') as $line){
+    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+        if (strpos($line, 'xy') !== false) {
+      echo "$line\n";
+    }
+  } 
+?>
